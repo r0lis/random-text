ae206b4f1b182c4bdee5c892ee61102b03311b29
Rolis <luky.rolenec@seznam.cz>
2021-02-08 10:00:02 +0100

    přidán celý projekt

---
+++ uv/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿
+namespace uv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Generate = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Generate
+            // 
+            this.Generate.Location = new System.Drawing.Point(63, 166);
+            this.Generate.Name = "Generate";
+            this.Generate.Size = new System.Drawing.Size(172, 41);
+            this.Generate.TabIndex = 0;
+            this.Generate.Text = "Generate";
+            this.Generate.UseVisualStyleBackColor = true;
+            this.Generate.Click += new System.EventHandler(this.Generate_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(63, 56);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(459, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 269);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Generate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Generate;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ uv/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uv
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random ran = new Random();
+        char[] letters = "irngniribfjkngiogturoafnngoti".ToArray();
+        string output;
+
+        void generate(int NoOFLetter)
+            
+        {
+            output = null;
+            for (int i=0; i< NoOFLetter; i++) {
+                output += letters[ran.Next(0, letters.Length)];
+            }
+            textBox1.Text = output;
+        }
+
+        private void Generate_Click(object sender, EventArgs e)
+        {
+            generate(4);
+        }
+    }
+}
